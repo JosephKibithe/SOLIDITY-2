8f21a4f91dd8f595dcb490b8e4ccacf36ac68a67
"Joseph Kibithe" <"Joekibithe2@gmail.com">
2022-07-09 02:49:25 +0300

    Array

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,12 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./SimpleStorage.sol";
+import "./SimpleStorage.sol"; //Do this instead of copying the whole Contract here
 contract StorageFactory {
-    SimpleStorage public simpleStorage; //type
+    SimpleStorage[] public simpleStorageArray; //simplestorae array
 
     function createSimpleStoraeContract() public {
-        //We can Copy the Simple CA underneath "pragma solidity ^0.8.0" and that will work
-        simpleStorage = new SimpleStorage(); //view of simple storage contract
+        //We can Copy the SimpleStorage CA underneath "pragma solidity ^0.8.0" and that will work
+       SimpleStorage simpleStorage = new SimpleStorage(); //view of simple storage contract
+       simpleStorageArray.push(simpleStorage); //To view an array
+    
+    }
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        //Address
+        //ABI(Application Binary Interface) - Tells code how to interact with CA
+        //to call store fn we need that contract object
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber); //Saving CA object in SimpleStorageIndex using simpleStorage variable
+        //Call store fn on store fn
+    }
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
     }
 }
